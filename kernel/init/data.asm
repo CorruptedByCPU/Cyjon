f50b153b6e9873a2755cff8c6aea7cba33e64031
Andrzej Adamczyk <53617677+Blackend@users.noreply.github.com>
2019-12-30 18:22:26 +0100

    http i shell jako procesy

--- kernel/init/data.asm
+++ kernel/init/data.asm
@@ -29,9 +29,11 @@ kernel_init_ap_count			db	STATIC_EMPTY
 kernel_init_apic_id_highest		db	STATIC_EMPTY
 
 kernel_init_services_list:
-	dq	service_network
 	dq	service_tresher
 	dq	service_tx
+	dq	service_network
+	dq	service_http
+	dq	service_shell
 
 	; koniec usług
 	dq	STATIC_EMPTY
