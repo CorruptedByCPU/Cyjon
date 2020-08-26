1a1f2b126bad3f7232cc4d95564a5b5b432e24f9
Andrzej Adamczyk <53617677+Blackend@users.noreply.github.com>
2020-08-26 12:48:39 +0200

    przystępniejsza wysokość elementów interfejsu oraz centrowanie tekstu w pionie

--- kernel/service/gui/config.asm
+++ kernel/service/gui/config.asm
@@ -2,8 +2,8 @@
 ; Copyright (C) by blackdev.org
 ;===============================================================================
 
-KERNEL_GUI_WINDOW_count			equ	3	; ilość okien utworzonych przez Cero
+KERNEL_GUI_WINDOW_count				equ	3	; ilość okien utworzonych przez Cero
 
 KERNEL_GUI_WINDOW_WORKBENCH_BACKGROUND_color	equ	0x00101010
 
-KERNEL_GUI_WINDOW_TASKBAR_HEIGHT_pixel	equ	LIBRARY_FONT_HEIGHT_pixel
+KERNEL_GUI_WINDOW_TASKBAR_HEIGHT_pixel		equ	18
