b68db33e89fc54ee06fe7d1a2c0ea196e20a22b4
Andrzej Adamczyk <53617677+Blackend@users.noreply.github.com>
2020-10-29 12:03:16 +0100

    console height fix

--- software/console/config.asm
+++ software/console/config.asm
@@ -6,11 +6,11 @@
 ;	Andrzej Adamczyk
 ;===============================================================================
 
-%define CONSOLE_VERSION			"0.24"
+%define CONSOLE_VERSION			"0.25"
 
 CONSOLE_WINDOW_WIDTH_char	equ	44
 CONSOLE_WINDOW_HEIGHT_char	equ	16
 CONSOLE_WINDOW_WIDTH_pixel	equ	LIBRARY_FONT_WIDTH_pixel * CONSOLE_WINDOW_WIDTH_char
-CONSOLE_WINDOW_HEIGHT_pixel	equ	LIBRARY_FONT_HEIGHT_pixel * CONSOLE_WINDOW_HEIGHT_char
+CONSOLE_WINDOW_HEIGHT_pixel	equ	(LIBRARY_FONT_HEIGHT_pixel * CONSOLE_WINDOW_HEIGHT_char) + LIBRARY_BOSU_ELEMENT_HEADER_HEIGHT_pixel
 
 CONSOLE_WINDOW_BACKGROUND_color	equ	0x00000000
