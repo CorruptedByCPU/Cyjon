6e8222c10403f76f4a4668916e3a5753808bf5d2
Andrzej Adamczyk <53617677+CorruptedByCPU@users.noreply.github.com>
2023-12-25 11:34:09 +0100

    fern-night dependence fix

--- make.sh
+++ make.sh
@@ -7,7 +7,7 @@ clear
 
 /bin/bash prepare.sh
 
-cp -rf ../fern-night/system/* system
+cp ../fern-night/build/system.gz iso
 
 xorriso -as mkisofs -b limine-cd.bin -no-emul-boot -boot-load-size 4 -boot-info-table --efi-boot limine-cd-efi.bin -efi-boot-part --efi-boot-image --protective-msdos-label iso -o build/cyjon.iso > /dev/null 2>&1
 
